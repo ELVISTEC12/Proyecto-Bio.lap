b86a6a0bb7fc34abc57cb97124f48ea2441b944b
Pablox2009 <pabloliquitaya2017@gmail.com>
2024-06-18 14:55:37 -0300

    Metodo de ingresar

--- Biolap/app/src/main/java/com/example/biolap/MainActivity.java
+++ Biolap/app/src/main/java/com/example/biolap/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.biolap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,30 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText usuarioTXT;
+    private EditText contraTXT;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        usuarioTXT = findViewById(R.id.usuarioTxt);
+        contraTXT = findViewById(R.id.contrasenaTxt);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom); 
             return insets;
         });
+    }
+    public void ingresar(View view){
+        String v1 = usuarioTXT.getText().toString();
+        String v2 = contraTXT.getText().toString();
+        if(v1.equals("Pablo") && v2.equals("riverplate")){
+            Intent m = new Intent(this,menup.class);
+            startActivity(m);
+        }
+        else{
+            Toast.makeText(this, "Los datos no coinciden", Toast.LENGTH_SHORT).show();
+        }
     }
 }
