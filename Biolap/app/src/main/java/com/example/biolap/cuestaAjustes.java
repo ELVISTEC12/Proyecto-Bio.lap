260e4f9b67a40dcd6acfd6e21e9e93bc9c78273d
ELVISTEC12 <126284708+ELVISTEC12@users.noreply.github.com>
2024-07-29 17:43:44 -0300

    cerrar sesion

--- Biolap/app/src/main/java/com/example/biolap/cuestaAjustes.java
+++ Biolap/app/src/main/java/com/example/biolap/cuestaAjustes.java
@@ -1,12 +1,17 @@
 package com.example.biolap;
 
 import android.annotation.SuppressLint;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -22,9 +27,11 @@ import com.android.volley.toolbox.Volley;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+
 public class cuestaAjustes extends AppCompatActivity {
 
     TextView name_user, correo, contra;
+    Button cerrar;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -36,6 +43,7 @@ public class cuestaAjustes extends AppCompatActivity {
         name_user = findViewById(R.id.name_user);
         correo = findViewById(R.id.correo_user);
         contra = findViewById(R.id.Contraseña_user);
+        cerrar = findViewById(R.id.cerrar);
 
         // Obtener el ID del usuario desde SharedPreferences
         SharedPreferences sharedPreferences = getSharedPreferences("MyAppPrefs", MODE_PRIVATE);
@@ -49,6 +57,13 @@ public class cuestaAjustes extends AppCompatActivity {
             // Manejar el caso de ID no válido
             Toast.makeText(this, "Usuario no encontrado", Toast.LENGTH_SHORT).show();
         }
+
+        cerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cerrar(v);
+            }
+        });
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -93,5 +108,29 @@ public class cuestaAjustes extends AppCompatActivity {
         // Añadir la solicitud a la cola de solicitudes
         RequestQueue requestQueue = Volley.newRequestQueue(this);
         requestQueue.add(stringRequest);
+    }
+
+    private void cerrar(View v) {
+        new AlertDialog.Builder(this)
+                .setTitle("Cerrar sesión")
+                .setMessage("¿Estás seguro de que deseas cerrar sesión?")
+                .setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        // Limpiar SharedPreferences
+                        SharedPreferences sharedPreferences = getSharedPreferences("MyAppPrefs", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.remove("USER_ID");
+                        editor.putBoolean("isUserLoggedIn", false);
+                        editor.apply();
+
+                        // Navegar a la actividad de inicio de sesión
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                        finish(); // Finaliza la actividad actual
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 }
--- Biolap/app/src/main/java/com/example/biolap/registrarUsuario.java
+++ Biolap/app/src/main/java/com/example/biolap/registrarUsuario.java
@@ -2,6 +2,7 @@ package com.example.biolap;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
@@ -26,6 +27,9 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -38,7 +42,18 @@ public class registrarUsuario extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        /*
+        SharedPreferences sharedPreferences = getSharedPreferences("MyAppPrefs", MODE_PRIVATE);
+        boolean isUserLoggedIn = sharedPreferences.getBoolean("isUserLoggedIn", false);
+
+        if (isUserLoggedIn) {
+            // Usuario ya está registrado/iniciado sesión, redirigir a menup
+            Intent intent = new Intent(this, menup.class);
+            startActivity(intent);
+            finish();
+            return;
+        }*/
+                EdgeToEdge.enable(this);
         setContentView(R.layout.activity_registrar_usuario);
         nombre = findViewById(R.id.nuser_r);
         correo = findViewById(R.id.correo_r);
@@ -51,12 +66,10 @@ public class registrarUsuario extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (validarDatos()) {
-                    //enviarDatos("http://192.168.1.4/bio.lap/insertar.php");
-                    enviarDatos("http://192.168.1.4/bio.lap/insertar.php");
+                    enviarDatos("http://192.168.1.3/bio.lap/insertar.php");
                 }
             }
         });
-
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -67,41 +80,35 @@ public class registrarUsuario extends AppCompatActivity {
 
     private boolean validarDatos() {
         String nombreUsuario = nombre.getText().toString().trim();
-        String correoUsuario = correo.getText().toString().trim();//Quita los espacios iniciales, finales y repetidos del texto.
+        String correoUsuario = correo.getText().toString().trim();
         String contrasena = contra.getText().toString().trim();
         String confirmacionContrasena = conta1.getText().toString().trim();
 
-        // Validar el campo de nombre
         if (TextUtils.isEmpty(nombreUsuario)) {
             nombre.setError("Campo obligatorio");
             return false;
         }
 
-        // Validar el correo electrónico
         if (TextUtils.isEmpty(correoUsuario) || !Patterns.EMAIL_ADDRESS.matcher(correoUsuario).matches()) {
             correo.setError("Correo electrónico inválido");
             return false;
         }
 
-        // Validar la contraseña
         if (TextUtils.isEmpty(contrasena)) {
             contra.setError("Campo obligatorio");
             return false;
         }
 
-        // Validar la confirmación de contraseña
         if (TextUtils.isEmpty(confirmacionContrasena)) {
             conta1.setError("Campo obligatorio");
             return false;
         }
 
-        // Verificar que las contraseñas coincidan
         if (!contrasena.equals(confirmacionContrasena)) {
             conta1.setError("Las contraseñas no coinciden");
             return false;
         }
 
-        // Verificar si el CheckBox está marcado
         if (check != null && !check.isChecked()) {
             Toast.makeText(this, "Debe aceptar los términos", Toast.LENGTH_SHORT).show();
             return false;
@@ -110,38 +117,55 @@ public class registrarUsuario extends AppCompatActivity {
         return true;
     }
 
-
     private void enviarDatos(String url) {
-      // Log.d("EnviarDatos", "Enviando datos a: " + url);
+        StringRequest sr = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    boolean success = jsonResponse.getBoolean("success");
 
-       StringRequest sr = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-           @Override
-           public void onResponse(String response) {
-             //  Log.d("EnviarDatos", "Respuesta: " + response);
-               Toast.makeText(getApplicationContext(), "Operación exitosa", Toast.LENGTH_SHORT).show();
-               Intent intent = new Intent(getApplicationContext(), menup.class);
-               startActivity(intent);
-           }
-       }, new Response.ErrorListener() {
-           @Override
-           public void onErrorResponse(VolleyError error) {
-               Log.e("EnviarDatos", "Error: " + error.toString());
-               Toast.makeText(getApplicationContext(), "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
-           }
-       }) {
-           @Override
-           protected Map<String, String> getParams() throws AuthFailureError {
-               Map<String, String> pa = new HashMap<String, String>();
-               pa.put("usuarios", nombre.getText().toString());
-               pa.put("contra", contra.getText().toString());
-               pa.put("correo", correo.getText().toString());
+                    if (success) {
+                        // Capturar el ID del usuario
+                        int userId = jsonResponse.getInt("user_id");
+                        Toast.makeText(getApplicationContext(), "Operación exitosa", Toast.LENGTH_SHORT).show();
 
-               return pa;
-           }
-       };
+                        // Guardar el ID del usuario en SharedPreferences
+                        SharedPreferences sharedPreferences = getSharedPreferences("MyAppPrefs", MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPreferences.edit();
+                        editor.putInt("USER_ID", userId);
+                        editor.apply();
 
-       RequestQueue requestQueue = Volley.newRequestQueue(this);
-       requestQueue.add(sr);
-   }
+                        // Navegar a la actividad de menú
+                        Intent intent = new Intent(getApplicationContext(), menup.class);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        Toast.makeText(getApplicationContext(), jsonResponse.getString("message"), Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "Error en la respuesta del servidor", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("EnviarDatos", "Error: " + error.toString());
+                Toast.makeText(getApplicationContext(), "Error: " + error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }) {
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> pa = new HashMap<>();
+                pa.put("usuarios", nombre.getText().toString());
+                pa.put("contra", contra.getText().toString());
+                pa.put("correo", correo.getText().toString());
+                return pa;
+            }
+        };
 
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(sr);
+    }
 }
