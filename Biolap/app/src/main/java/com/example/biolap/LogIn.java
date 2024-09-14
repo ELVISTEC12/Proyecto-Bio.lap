51283770f3151ca655679ab3c62ad96099f2816c
Pablox2009 <pabloliquitaya2017@gmail.com>
2024-09-14 19:01:11 -0300

    más cambios

--- Biolap/app/src/main/java/com/example/biolap/LogIn.java
+++ Biolap/app/src/main/java/com/example/biolap/LogIn.java
@@ -3,7 +3,6 @@ package com.example.biolap;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,8 +16,7 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.example.biolap.conexion.Conexion;
-import com.example.biolap.sqlCod.usuarioCod;
+import com.example.biolap.modelo.usuarioData;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.Nullable;
@@ -27,6 +25,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -34,9 +35,8 @@ public class LogIn extends AppCompatActivity {
     EditText usuarioTXT;
     EditText contraTXT;
     Button ing;
-    String usuario,contrasena;
     TextView errorT;
-    boolean verificared=true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,8 +51,7 @@ public class LogIn extends AppCompatActivity {
         ing.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                //enviarDatos("http://bio-lab.byethost13.com/validar_usuario.php");
-                validar();
+                enviarDatos("http://192.168.0.108/bio.lap/validar_usuario.php");
             }
         });
 
@@ -63,64 +62,47 @@ public class LogIn extends AppCompatActivity {
         });
     }
 
-
-    public boolean validar(){
-        usuario = usuarioTXT.getText().toString().trim();
-        contrasena = contraTXT.getText().toString().trim();
-        if(TextUtils.isEmpty(usuario)){
-            usuarioTXT.setError("Campo obligatorio");
-            return false;
-        }
-        if(TextUtils.isEmpty(contrasena)){
-            contraTXT.setError("Campo obligatorio");
-            return false;
-        }
-        usuarioCod uc = new usuarioCod();
-        boolean v = uc.verificar(usuario,contrasena);
-        if(!v){
-            Intent mp = new Intent(this, menuPrincipal.class);
-            startActivity(mp);
-        }
-        else{
-            Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
-        }
-        return true;
-    }
     public void enviarDatos(String URL){
-        boolean verificacion = validar();
-        if (verificacion){
-            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-                    if(!response.isEmpty()){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    boolean success = jsonResponse.getBoolean("success");
+                    if (success) {
+                        int id = jsonResponse.getInt("id");
+                        String nombre = jsonResponse.getString("nombre");
+                        usuarioData ud = new usuarioData();
+                        ud.setNombre(nombre);
+                        ud.setUsuarioId(id);
                         Intent mp = new Intent(getApplicationContext(), menuPrincipal.class);
+                        mp.putExtra("nombre", ud.getNombre());
                         startActivity(mp);
+                    } else {
+                        Toast.makeText(LogIn.this, "No se encontraron coincidencias", Toast.LENGTH_SHORT).show();
                     }
-                    else{
-                        Toast.makeText(LogIn.this, "Datos erroneos", Toast.LENGTH_SHORT).show();
-                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                    Toast.makeText(LogIn.this, "Error en el servidor", Toast.LENGTH_SHORT).show();
                 }
-            }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    errorT.setText(error.toString());
-                }
-            }){
-                @Nullable
-                @Override
-                protected Map<String, String> getParams() throws AuthFailureError {
-                    Map<String,String>parametros= new HashMap<String,String>();
-                    parametros.put("nombre",usuarioTXT.getText().toString());
-                    parametros.put("contra",contraTXT.getText().toString());
-                    return parametros;
-                }
-            };
-            RequestQueue requestQueue = Volley.newRequestQueue(this);
-            requestQueue.add(stringRequest);
-        }
-        else{
-            Toast.makeText(this, "Error en validacion", Toast.LENGTH_SHORT).show();
-        }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                errorT.setText(error.toString());
+            }
+        }) {
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<>();
+                parametros.put("usuario", usuarioTXT.getText().toString());
+                parametros.put("clave", contraTXT.getText().toString());
+                return parametros;
+            }
+        };
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        requestQueue.add(stringRequest);
     }
 
     public void registrar(View view) {
--- Biolap/app/src/main/java/com/example/biolap/ajustes.java
+++ Biolap/app/src/main/java/com/example/biolap/ajustes.java
@@ -1,8 +1,11 @@
 package com.example.biolap;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,18 +13,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class ajustes extends AppCompatActivity {
+import com.example.biolap.modelo.usuarioData;
 
+public class ajustes extends AppCompatActivity {
+    private TextView nombre;
+    private String v;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ajustes);
+        //nombre = findViewById(R.id.u);
+        Intent intent = getIntent();
+        v = intent.getStringExtra("nombre");
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void cam(View view){
+        Toast.makeText(this, "nombre: "+v, Toast.LENGTH_SHORT).show();
     }
     public void cuenta(View view){
         Intent c = new Intent(this, perfilAjuste.class);
